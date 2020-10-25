5bb4900929f7d27b500a9e3d0b009607b81d6712
Rui Cui <cruilnsy@gmail.com>
2020-10-24 23:34:10 -0700

    Added new collections for part3 module4 assignment

--- config/database.js
+++ config/database.js
@@ -1,33 +1,33 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'blog'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', '*6BB4837EB74329105EE4568DDA7DC67ED2CA2AD9'),
-      },
-      options: {},
-    },
-  },
-});
-
 // module.exports = ({ env }) => ({
 //   defaultConnection: 'default',
 //   connections: {
 //     default: {
 //       connector: 'bookshelf',
 //       settings: {
-//         client: 'sqlite',
-//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//         client: 'mysql',
+//         host: env('DATABASE_HOST', 'localhost'),
+//         port: env.int('DATABASE_PORT', 3306),
+//         database: env('DATABASE_NAME', 'blog'),
+//         username: env('DATABASE_USERNAME', 'root'),
+//         password: env('DATABASE_PASSWORD', '*6BB4837EB74329105EE4568DDA7DC67ED2CA2AD9'),
 //       },
-//       options: {
-//         useNullAsDefault: true,
-//       },
+//       options: {},
 //     },
 //   },
 // });
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+  },
+});
